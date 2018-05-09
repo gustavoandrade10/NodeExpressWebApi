98c1ace8ebed0750a7b6472212d2dd4e7f6c1bab
Talisson Junior <talisson.carvalho@prodemge.gov.br>
2018-05-09 13:07:46 -0300

    Update: Updated resources - database and postman.

--- resources/db_newa.sql
+++ resources/db_newa.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `db_newa` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `db_newa`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: db_newa
+-- Host: 127.0.0.1    Database: db_newa
 -- ------------------------------------------------------
--- Server version	5.7.19-log
+-- Server version	5.5.57
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,10 +26,10 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(45) DEFAULT NULL,
-  `lastname` varchar(11) DEFAULT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
   `phone` varchar(11) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -44,36 +44,6 @@ LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
 INSERT INTO `employee` VALUES (2,'Big','BOSS','936854270','big.boss@company.com','2018-05-08 00:00:00','2018-05-08 00:00:00'),(3,'Jonas','Xavier','936854271','jonas.xavier@company.com','2018-05-08 00:00:00','2018-05-08 00:00:00'),(4,'Linus','TORVALDS','936854272','linus.torvalds@company.com','2018-05-08 00:00:00','2018-05-08 00:00:00'),(5,'Jimmy','WALES','936854273','jimmy.wales@company.com','2018-05-08 00:00:00','2018-05-08 00:00:00'),(6,'Larry','PAGE','936854274','larry.page@company.com','2018-05-08 00:00:00','2018-05-08 00:00:00');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `employee_project`
---
-
-DROP TABLE IF EXISTS `employee_project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `employee_project` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_project` int(11) NOT NULL,
-  `id_employee` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_employee_project_id_project` (`id_project`),
-  KEY `fk_employee_project_id_employee` (`id_employee`),
-  CONSTRAINT `fk_employee_project_id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`),
-  CONSTRAINT `fk_employee_project_id_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employee_project`
---
-
-LOCK TABLES `employee_project` WRITE;
-/*!40000 ALTER TABLE `employee_project` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employee_project` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -108,6 +78,64 @@ INSERT INTO `project` VALUES (3,'Google','1998-09-08',NULL,5,'2018-05-08 18:26:4
 UNLOCK TABLES;
 
 --
+-- Table structure for table `project_employee`
+--
+
+DROP TABLE IF EXISTS `project_employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_project` int(11) NOT NULL,
+  `id_employee` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_project_employee_id_project` (`id_project`),
+  KEY `fk_project_employee_id_employee` (`id_employee`),
+  CONSTRAINT `fk_project_employee_id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`),
+  CONSTRAINT `fk_project_employee_id_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_employee`
+--
+
+LOCK TABLES `project_employee` WRITE;
+/*!40000 ALTER TABLE `project_employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Talisson junior','juniortalisson16@gmail.com','teste','2018-05-09 14:14:16','2018-05-09 14:14:16');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'db_newa'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -120,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-08 21:40:00
+-- Dump completed on 2018-05-09 13:07:10
